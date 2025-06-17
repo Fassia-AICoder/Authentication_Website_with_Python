a3447442fdb07934dc07a5f49eeb10f7ff587f30
Fassia-AICoder <fassiaouanre@gmail.com>
2025-06-17 17:19:57 +0000

    Creation of templates(navbar)

--- website/auth.py
+++ website/auth.py
@@ -1,14 +1,14 @@
 from flask import Blueprint
 """Blueprints are used to group related routes, templates, and static files, 
 making it easier to organize large applications or reuse code across multiple projects."""
-
+from flask import render_template
 
 #This line creates an instance of the Blueprint class
 auth = Blueprint('auth', __name__) #the second auth in argument is the name of my blueprint to represent it 
 
 @auth.route('/login')
 def login():
-    return "<p>login</p>" 
+    return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
@@ -16,4 +16,4 @@ def logout():
 
 @auth.route('/sign-up')
 def sign_up():
-    return "<p>Sign Up</p>" 
+    return render_template("sign_up.html") 
--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from flask import Blueprint #import the Blueprint class from the flask module
 """Blueprints are used to group related routes, templates, and static files, 
 making it easier to organize large applications or reuse code across multiple projects."""
+from flask import render_template 
 
 #This line creates an instance of the Blueprint class
 views = Blueprint('views', __name__) #the second views in argument is the name of my blueprint to represent it 
@@ -8,4 +9,4 @@ views = Blueprint('views', __name__) #the second views in argument is the name o
 #create a view or a route 
 @views.route('/')
 def home():
-    return "<h1>This is my first website</h1>" 
+    return render_template("home.html")
